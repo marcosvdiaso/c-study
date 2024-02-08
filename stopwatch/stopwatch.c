e58ca926ab51273011bdbd15d04cb92e4afdcba8
Marcos Dias <marcosvdiaso@gmail.com>
2024-02-08 12:02:54 -0300

    Create stopwatch.c

---
+++ stopwatch/stopwatch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<windows.h> // Library with sleep and cls
+
+    void clearScreen() { // Function that clear the screen
+    system("cls");
+    }
+
+int main(){
+    int sw; // Variable declaration sw (sw is stopwatch shortcut)
+
+    printf("Stopwatch.\n\n");
+    printf("How many seconds?\n");
+    scanf("%d", &sw); // Asks the user how many seconds
+
+    while (sw != -1){ /* Loop with 1. Clear Screen, 2. Print the actual second
+        3. Wait 1000 milliseconds, 4. decreases the value of sw and repeats */
+        clearScreen();
+        printf("%d\n", sw);
+        Sleep(1000);
+        sw--;
+    }
+}
